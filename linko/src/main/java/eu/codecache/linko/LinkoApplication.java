1bf1f2c8ea8d8564741c895490e6d87f779d7cde
kosvi <ville.koskela@kv1.fi>
2021-02-01 19:25:49 +0200

    Added Spring project template

---
+++ linko/src/main/java/eu/codecache/linko/LinkoApplication.java
@@ -0,0 +1,13 @@
+package eu.codecache.linko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkoApplication.class, args);
+	}
+
+}
---
+++ linko/src/test/java/eu/codecache/linko/LinkoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.codecache.linko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
