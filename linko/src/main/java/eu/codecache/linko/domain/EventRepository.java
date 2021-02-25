e3e61f4a12b59920f16fedc1bab360bc96aa0b4b
Joni Nikkanen <a1800305@myy.haaga-helia.fi>
2021-02-26 01:32:46 +0200

    Tehty PUT ja POST

--- linko/src/main/java/eu/codecache/linko/domain/EventRepository.java
+++ linko/src/main/java/eu/codecache/linko/domain/EventRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
 	
+	<S extends Event> S save(S event);
+	
 	void deleteById(Long EventID);
+	
 
+	
 }
--- linko/src/main/java/eu/codecache/linko/web/EventController.java
+++ linko/src/main/java/eu/codecache/linko/web/EventController.java
@@ -2,14 +2,24 @@ package eu.codecache.linko.web;
 
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import eu.codecache.linko.domain.Event;
 import eu.codecache.linko.domain.EventRepository;
@@ -32,6 +42,15 @@ public class EventController {
 		return (List<Event>) repository.findAll();
 	}
 	
+	@PostMapping("/events")
+		Event newEmployee(@RequestBody Event newEvent) {
+		return repository.save(newEvent);
+	  }
+	
+	@PutMapping(path="/events")
+	public Event update(@RequestBody Event eventID) {
+	    return repository.save(eventID);
+	}
 	
 	// Delete-toiminnallisuus:
 	 @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET) //{id} is the path variable. you can delete by localhost/8080/idnumber
