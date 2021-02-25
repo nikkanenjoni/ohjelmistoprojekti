eb8c1b5af2b172c751e3cb0116f9ff59058f1043
a1800300 <sofia.lindholm@myy.haaga-helia.fi>
2021-02-25 20:08:01 +0200

    ei muutoksia

--- linko/src/main/java/eu/codecache/linko/web/EventController.java
+++ linko/src/main/java/eu/codecache/linko/web/EventController.java
@@ -3,6 +3,9 @@ package eu.codecache.linko.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventController {
@@ -11,4 +14,5 @@ public class EventController {
 	public String test(Model model) {
 		return "test";
 	}
+	  
 }
