d2bb6e8aaceca51644114be273277978895181f9
a1800300 <sofia.lindholm@myy.haaga-helia.fi>
2021-03-01 19:27:51 +0200

    TicketControllerin aloitus

--- linko/src/main/java/eu/codecache/linko/web/TicketController.java
+++ linko/src/main/java/eu/codecache/linko/web/TicketController.java
@@ -1,13 +1,34 @@
 package eu.codecache.linko.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import eu.codecache.linko.domain.Event;
 import eu.codecache.linko.domain.OrderRepository;
+import eu.codecache.linko.domain.Ticket;
+import eu.codecache.linko.domain.TicketOrderRepository;
+import eu.codecache.linko.domain.TicketRepository;
 
 @Controller
 public class TicketController {
+	
+	
+	//controlleriin liittyvät repot (alustavasti)
+	@Autowired
+	public OrderRepository orderRepo;
+	public TicketRepository ticketRepo;
+	public TicketOrderRepository ticketorderRepo;
+	
+	// displays ALL tickets in the database
+	@GetMapping("/api/tickets")
+	public @ResponseBody List<Ticket> all() {
+		return ticketRepo.findAll();
+	}
+	
 	
 }
