5d9d536e93bb33e5b74cb84622706d4843ed88e2
Joni Nikkanen <a1800305@myy.haaga-helia.fi>
2021-03-04 21:11:35 +0200

    Lisätty ticketrepo ja ticketcontroller

--- linko/src/main/java/eu/codecache/linko/domain/TicketRepository.java
+++ linko/src/main/java/eu/codecache/linko/domain/TicketRepository.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import eu.codecache.linko.exception.TicketNotFoundException;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-	
+
+	// get-toiminnallisuus ( hae kaikki liput )
+	List<Ticket> findAll();
+
+	// get, yksi lippu
+	// this is not working as intented, if eventID is not found,
+	// it return empty event instead of throwing EventNotFoundException
+	// ... I wonder if this could be fixed somehow?
+	Ticket findByTicketID(long ticketID) throws TicketNotFoundException;
+
+	// delete-toiminnallisuus
+	void deleteById(long ticketID);
+
 }
--- linko/src/main/java/eu/codecache/linko/web/TicketController.java
+++ linko/src/main/java/eu/codecache/linko/web/TicketController.java
@@ -1,13 +1,89 @@
 package eu.codecache.linko.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import eu.codecache.linko.domain.CityRepository;
+import eu.codecache.linko.domain.Event;
+import eu.codecache.linko.domain.EventRepository;
 import eu.codecache.linko.domain.OrderRepository;
+import eu.codecache.linko.domain.Ticket;
+import eu.codecache.linko.domain.TicketRepository;
+import eu.codecache.linko.exception.EventNotFoundException;
+import eu.codecache.linko.exception.TicketNotFoundException;
 
-@Controller
+@RestController
 public class TicketController {
-	
+	@Autowired
+	public TicketRepository repository;
+	public CityRepository cityrepository;
+
+	// displays ALL events in the database
+	@GetMapping("/api/tickets")
+	public @ResponseBody List<Ticket> all() {
+		return repository.findAll();
+	}
+
+	@PostMapping("/api/tickets")
+	public @ResponseBody Ticket newTicket(@RequestBody Ticket ticket) {
+		repository.save(ticket);
+
+		return ticket;
+	}
+
+	@PutMapping("/api/tickets/{id}")
+	public @ResponseBody Ticket updateTicket(@PathVariable("id") Long ticketID, @RequestBody Ticket ticket) {
+		// first let's see if we have an event with the id
+		try {
+			Ticket dbTicket = repository.findByTicketID(ticketID);
+			// ok, we have found the event
+			// update it with the new information
+			dbTicket.setTicketType(ticket.getTicketType());
+			dbTicket.setEvent(ticket.getEvent());
+			dbTicket.setDescription(ticket.getDescription());
+			// now we should be able to (over)write to database without accidentally
+			// creating a new event
+			repository.save(dbTicket);
+			// return the updated event
+			return dbTicket;
+		} catch (TicketNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found");
+		}
+	}
+
+	// näytä yksi tapahtuma
+	// Single item
+	@GetMapping("/api/tickets/{id}")
+	public @ResponseBody Ticket findTicket(@PathVariable("id") Long ticketID) {
+
+		try {
+			return repository.findByTicketID(ticketID);
+		} catch (TicketNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found");
+		}
+	}
+
+	// Delete-toiminnallisuus:
+	@RequestMapping(value = "/api/tickets/delete/{id}", method = RequestMethod.GET) // {id} is the path variable. you can
+																					// delete by localhost/8080/idnumber
+	public String deleteTicket(@PathVariable("id") Long ticketID, Model model) { // saves it to the variable ticketID
+		repository.deleteById(ticketID);
+		return "Ticket deleted";
+	}
+
 }
+
