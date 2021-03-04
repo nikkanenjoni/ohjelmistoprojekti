7983684dca5376985112746380b078f0d90634e8
a1800300 <sofia.lindholm@myy.haaga-helia.fi>
2021-03-04 20:56:35 +0200

    lisätty lipputestidataa LinkoApplication.java

--- linko/src/main/java/eu/codecache/linko/domain/TicketTypeRepository.java
+++ linko/src/main/java/eu/codecache/linko/domain/TicketTypeRepository.java
@@ -5,5 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketTypeRepository extends JpaRepository<TicketType, Long> {
+	
+	// We need to be able to get a single tickettype out from the database (Viittaa Application-sivuun)
+	List<TicketType> findTicketTypeByTicketType(String ticketType);
 
 }
