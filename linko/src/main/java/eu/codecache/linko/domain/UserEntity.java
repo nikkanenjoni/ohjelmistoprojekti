d895210276db33d1302c710006a7e644ae6b8adf
a1800300 <sofia.lindholm@myy.haaga-helia.fi>
2021-04-01 21:12:16 +0300

    user ja authorization entityt

--- linko/src/main/java/eu/codecache/linko/domain/UserEntity.java
+++ linko/src/main/java/eu/codecache/linko/domain/UserEntity.java
@@ -1,5 +1,25 @@
 package eu.codecache.linko.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UserEntity {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(name = "userID", nullable = false, updatable = false)
+ private Long userID;
+ 
+ 
+// Username with unique constraint (if needed)
+ @Column(name = "username", nullable = false, unique = true)
+ private String username;
+ 
+ 
+ @Column(name = "password", nullable = false)
+ private String passwordHash;
 
 }
