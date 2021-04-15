4d912ddb1a98924a69cfefbf8787e16f97bc6743
kosvi <ville.koskela@kv1.fi>
2021-04-15 13:58:52 +0300

    Fixes to CORS

--- linko/src/main/java/eu/codecache/linko/web/TicketOrderController.java
+++ linko/src/main/java/eu/codecache/linko/web/TicketOrderController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import eu.codecache.linko.domain.TicketOrder;
 import eu.codecache.linko.domain.TicketOrderRepository;
 import eu.codecache.linko.domain.UserEntityRepository;
 
+@CrossOrigin(origins="*", allowedHeaders="*")
 @RestController
 public class TicketOrderController {
 
