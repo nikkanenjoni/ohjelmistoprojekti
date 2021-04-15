86f1545ee73489c4f4f9d02a5b999d8464cc0cd5
kosvi <ville.koskela@kv1.fi>
2021-04-15 13:55:48 +0300

    working on api-api :)

--- ticketguru/src/classes/DatabaseAccessApi.js
+++ ticketguru/src/classes/DatabaseAccessApi.js
@@ -2,14 +2,53 @@ import { Log } from "./Log.js";
 
 export class DatabaseAccessApi {
 
+    static setUser(username, password) {
+        InternalMethods.setUser(username, password);
+    }
+
+    static async getEvents() {
+        const response = await InternalMethods.getData("https://ticketguru.codecache.eu/api/events");
+        console.log(response);
+    }
 }
 
 class InternalMethods {
 
-    static #user = "admin";
+    static className = "DatabaseAccessApi.InternalMethods";
+    static #username = "admin";
     static #password = "password";
+
+    static setUser(username, password) {
+        this.#username = username;
+        this.#password = password;
+    }
+
+    static getUserBase64() {
+        return Buffer.from(this.#username + ":" + this.#password);
+    }
+
+    static createHeaders() {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Authorization', 'Basic ' + this.getUserBase64());
+        return headers;
+    }
 
     /*
      *  These are the actual methods responsible of the fetch-calls
      */
+
+    static async getData(url) {
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: this.createHeaders(),
+            });
+            const responseJson = await response.json();
+            return responseJson;
+        } catch (error) {
+            Log.writeLog(this.className + ".getData(): " + error, 1);
+            return null;
+        }
+    }
 }
